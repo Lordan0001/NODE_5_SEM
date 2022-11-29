caf294691da20499a37e31411fce7ec0148c6033
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2022-11-29 04:10:01 +0300

    all fixed

--- LAB_11/6a.js
+++ LAB_11/6a.js
@@ -10,20 +10,3 @@ wsA.on('open',()=>{
     });
 });
 
-let wsB = new rpcWSC('ws://localhost:4000');
-wsB.on('open',()=>{
-    wsB.subscribe('B');
-    wsB.on('B',()=>
-    {
-        console.log('Event B');
-    });
-});
-
-let wsC = new rpcWSC('ws://localhost:4000');
-wsC.on('open',()=>{
-    wsC.subscribe('C');
-    wsC.on('C',()=>
-    {
-        console.log('Event C');
-    });
-});
