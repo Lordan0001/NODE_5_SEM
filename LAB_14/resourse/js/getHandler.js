12f1cc33f637c24b018630a26e1203c6b523c7d1
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2022-12-24 09:00:15 +0300

    with 2 cringe req

---
+++ LAB_14/resourse/js/getHandler.js
@@ -0,0 +1,87 @@
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+
+const Db = require('./db')
+let DB = new Db();
+module.exports = (req, res) => {
+    let path = url.parse(req.url).pathname;
+    let path_params = path.split('/');
+
+
+    switch(true)
+    {
+        case path == '/': 
+            res.end(fs.readFileSync('./resourse/views/index.html'));
+            break;
+        case path == '/api/faculties': 
+        DB.getFaculties().then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+            break;
+        case path == '/api/pulpits':
+        DB.getPulpits().then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+            break;
+        case path == '/api/subjects': 
+        DB.getSubjects().then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+            break;
+        case path == '/api/auditoriumtypes': 
+        DB.getAuditoriumsTypes().then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+        break;
+        case path == '/api/auditoriums':
+        DB.getAuditoriums().then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+        break;
+
+        case '/api/auditoriums':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            DB.getFaculty(path_params[3]).
+            then((res)=>{if(res.recordset.length < 0) throw 'No such auditorium'}).
+            catch(error=>{write_error_400(res,error)});
+            DB.deleteAuditoriums(path_params[3]).then(records => {
+                res.end('deleted')
+            }).catch(error => {write_error_400(res, error)});
+            break;
+
+
+//refactor  1
+        //case path == '/api/faculty/xyz/pulpits': 
+       // case path ==  '^\/api\/faculty\/[a-zA-z0-9]+\/pulpits$':
+       // /api/faculty/ИДиП/pulpits
+       case path == '/api/faculty/%D0%98%D0%94%D0%B8%D0%9F/pulpits':
+        DB.facultyPulpitsJoin(path_params[3]).then(records => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(records.recordset))
+        }).catch(error => {write_error_400(res, error)});
+            break;
+
+//refactor  2
+case path == '/api/auditoriumtypes/%D0%9B%D0%9A/auditoriums': 
+// /api/auditoriumtypes/ЛК/auditoriums
+DB.auditoriumtypesAuditoriumsJoin().then(records => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(records.recordset))
+}).catch(error => {write_error_400(res, error)});
+    break;
+
+    }
+}
